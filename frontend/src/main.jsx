e6551ceeeba4fe82e222f9632def218f7548f3e7
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-02-03 02:19:07 +0530

    initialize the folder structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/main.layout.jsx";
+import Home from "./pages/Home/Home.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  { element: <MainLayout />, children: [{ path: "/", element: <Home /> }] },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </React.StrictMode>
+);
