c2b1167bfe25c099b95441884a5f36b2ce76d8d3
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-02-05 13:51:16 +0530

    increase box shadow of the navigation bar

--- frontend/src/components/shared/NavigationBar.jsx
+++ frontend/src/components/shared/NavigationBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const NavigationBar = () => {
   return (
-    <div className="block w-full bg-[#020817]  p-5 text-center sticky  top-0 shadow-lg ring-1 ring-black/5">
+    <div className="block w-full bg-[#020817]  p-5 text-center sticky  top-0 shadow-2xl ring-1 ring-black/5">
       <Link className="text-4xl font-medium text-underlay-1 ">Library MS</Link>
     </div>
   );
