4dd6dc620172b4dd2891fc7ec61ac2e43bb4a8b6
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-02-05 13:28:46 +0530

    implement add new book function btn

---
+++ frontend/src/components/shared/AddBookFloatingIcon.jsx
@@ -0,0 +1,38 @@
+import { PlusIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+const calculateWindowSize = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+
+  return {
+    width,
+    height,
+  };
+};
+const AddBookFloatingIcon = ({ onClick }) => {
+  const [windowDimensions, setWindowDimensions] = useState(
+    calculateWindowSize()
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions(calculateWindowSize());
+    };
+
+    addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <Link to={'/books/add'}>
+      <div className="group bg-[#7aaaff] rounded-full hover:transition-all  md:p-3 cursor-pointer shadow-light-shadow">
+        <PlusIcon
+          size={windowDimensions.width <= 640 ? 40 : 50}
+          className="group hover:rotate-180 duration-500"
+        />
+      </div>
+    </Link>
+  );
+};
+
+export default AddBookFloatingIcon;
