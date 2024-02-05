faa38a38e78666abde971902a5fc956641a264a0
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-02-05 17:29:23 +0530

    implemented the backend to server files after host

--- backend/index.js
+++ backend/index.js
@@ -33,9 +33,20 @@ app.use("/api/books", BookRoutes);
 // upload routes
 app.use("/api/uploads", UploadRoutes);
 
-const __dirname = path.resolve("../"); //set __dirname to root directory
+const __dirname = path.resolve(""); //set __dirname to root directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("Lib Management system API is responsing...");
+  });
+}
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server up and running ${PORT}`);
--- backend/routes/upload.routes.js
+++ backend/routes/upload.routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // upload routes
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "../uploads/");
+    cb(null, "uploads/");
   },
   filename(req, file, cb) {
     cb(
