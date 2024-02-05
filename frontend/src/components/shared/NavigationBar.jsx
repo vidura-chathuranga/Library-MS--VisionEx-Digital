6d576d675e8889351c76eb55819ae5a2a83be654
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-02-05 13:48:39 +0530

    implemented sticky header with Logo

---
+++ frontend/src/components/shared/NavigationBar.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export const NavigationBar = () => {
+  return (
+    <div className="block w-full bg-[#020817]  p-5 text-center sticky  top-0 shadow-lg ring-1 ring-black/5">
+      <Link className="text-4xl font-medium text-underlay-1 ">Library MS</Link>
+    </div>
+  );
+};
+export default NavigationBar;
--- frontend/src/layouts/main.layout.jsx
+++ frontend/src/layouts/main.layout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
+import NavigationBar from "@/components/shared/NavigationBar";
 const MainLayout = () => {
   return (
     <main className="container">
+      <NavigationBar />
       <Outlet />
       <Toaster />
     </main>
